b1544c45c813485e45eb36e5d6571cc616107aa9
josanuz <josanuz@hotmail.es>
2014-05-18 17:00:45 -0600

    lots

--- web/js/validation.js
+++ web/js/validation.js
@@ -1,48 +1,83 @@
-function newStudentForm(){
+function newStudentForm() {
 
-var carnet = document.forms["newStudent"]["pCarnet"].value;
-var surName = document.forms["newStudent"]["pSurName"].value;
-var name = document.forms["newStudent"]["pName"].value;
-if (carnet===null || carnet==="")
-    alert("Debe Indicar El Carnet");
-else if (surName===null || surName==="")
-    alert("Debe Indicar los Apellidos");
-else if (name===null || name==="")
-    alert("Debe Indicar El Nombre");
-else document.forms["newStudent"].submit();
-};
+    var carnet = document.forms["newStudent"]["pCarnet"].value;
+    var surName = document.forms["newStudent"]["pSurName"].value;
+    var name = document.forms["newStudent"]["pName"].value;
+    if (carnet === null || carnet === "")
+        alert("Debe Indicar El Carnet");
+    else if (surName === null || surName === "")
+        alert("Debe Indicar los Apellidos");
+    else if (name === null || name === "")
+        alert("Debe Indicar El Nombre");
+    else
+        document.forms["newStudent"].submit();
+}
+;
 
-function effect(ele){
+function effect(ele) {
+    //a = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
     e = document.getElementById(ele);
-    e.style.color= "red";
+    //e.style.color = "red";
     e.style.fontWeight = "bold";
-    e.style.textDecoration ="underline";
-};
-function deefect(e){
-    e.style.color= "black";
+    e.style.textDecoration = "underline";
+    jele = "#"+ele;
+   $(jele).animate({color : "#FF0000", opacity: 1}, 400, function() { });
+    //$("#"+ele).animate({color: '#FF0000'});
+}
+;
+function deefect(e) {
+    ///e.style.color = "black";
     e.style.fontWeight = "normal";
-    e.style.textDecoration ="none";
+    e.style.textDecoration = "none";
+    $(e).animate({color : "#000000", opacity: 0.75}, 400, function() { });
 }
-function DeleteStudent(){
-    alert("TODO");
-}
-function UpdateStudent(id){
-  var myForm = document.createElement("form");
-  myForm.method="post" ;
-  myForm.action = "UpdateStudent";
- var _id = document.createElement("input") ; var _t1 = document.createElement("input") ; 
- var _t2 = document.createElement("input") ; var _t3 = document.createElement("input") ;
- var arr = new Array(3);
- arr[0] = document.getElementById("1"+id).value;
- arr[1] = document.getElementById("2"+id).value;
- arr[2] = document.getElementById("3"+id).value;
+function DeleteStudent(id) {
+    
+    var myForm = document.createElement("form");
+    myForm.method = "post";
+    myForm.action = "DeleteStudent";
+    var _id = document.createElement("input");
 
- _id.setAttribute("name", "pCarnet") ; _id.setAttribute("value", id);  myForm.appendChild(_id) ;
- _t1.setAttribute("name", "pt1") ;     _t1.setAttribute("value", arr[0]); myForm.appendChild(_t1) ;
- _t2.setAttribute("name", "pt2") ;     _t2.setAttribute("value", arr[1]); myForm.appendChild(_t2) ;
- _t3.setAttribute("name", "pt3") ;     _t3.setAttribute("value", arr[2]);  myForm.appendChild(_t3) ;
-  
-  document.body.appendChild(myForm) ;
-  myForm.submit() ;
-  document.body.removeChild(myForm) ;
+    _id.setAttribute("name", "pCarnet");
+    _id.setAttribute("value", id);
+    myForm.appendChild(_id);
+
+    document.body.appendChild(myForm);
+    myForm.submit();
+    document.body.removeChild(myForm);
+}
+function UpdateStudent(id) {
+    var myForm = document.createElement("form");
+    myForm.method = "post";
+    myForm.action = "UpdateStudent";
+    var _id = document.createElement("input");
+    var _t1 = document.createElement("input");
+    var _t2 = document.createElement("input");
+    var _t3 = document.createElement("input");
+    var arr = new Array(3);
+    arr[0] = document.getElementById("1" + id).value;
+    arr[1] = document.getElementById("2" + id).value;
+    arr[2] = document.getElementById("3" + id).value;
+
+    _id.setAttribute("name", "pCarnet");
+    _id.setAttribute("value", id);
+    myForm.appendChild(_id);
+    _t1.setAttribute("name", "pt1");
+    _t1.setAttribute("value", arr[0]);
+    myForm.appendChild(_t1);
+    _t2.setAttribute("name", "pt2");
+    _t2.setAttribute("value", arr[1]);
+    myForm.appendChild(_t2);
+    _t3.setAttribute("name", "pt3");
+    _t3.setAttribute("value", arr[2]);
+    myForm.appendChild(_t3);
+
+    document.body.appendChild(myForm);
+    myForm.submit();
+    document.body.removeChild(myForm);
+}
+function begin() {
+    //document.getElementById("wrapper").style.height = (window.innerHeight - 10) + "px";
+    val = document.getElementById("dvMsj").innerHTML;
+    if(!(val ==="" || val === null)) alert(val); 
 }
